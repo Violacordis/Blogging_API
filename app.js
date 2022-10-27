98f806640318276d71c676b7563933fe99d91f32
Violacordis <ikemviolacordis@gmail.com>
2022-10-27 11:33:49 +0100

    Defined all error handlers including try catch error, error middleware that i will be using on this project

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const db = require("./db/db_connect");
+const errorHandler = require("./controllers/errorController");
+const AppError = require("./utils/appError");
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log("Starting my exam!!!");
@@ -10,4 +14,13 @@ app.get("/", (req, res) => {
     message: "Welcome to my Blog Website",
   });
 });
+
+// catching all  undefined routes
+app.all("*", (req, res, next) => {
+  next(new AppError(`Page not found!`, 404));
+});
+
+// Error handling middleware
+app.use(errorHandler);
+
 module.exports = app;
