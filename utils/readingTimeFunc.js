6a65d599f95fda67a6b61b4aee3f3493b1a397ab
Violacordis <ikemviolacordis@gmail.com>
2022-11-04 10:51:10 +0100

    file restructuring

--- utils/readingTimeFunc.js
+++
@@ -1,9 +0,0 @@
-function readingTime(body) {
-  const wordsPerMinute = 200;
-  const noOfWords = body.split(" ").length;
-  const minutes = noOfWords / wordsPerMinute;
-  const readTime = Math.ceil(minutes);
-  return readTime;
-}
-
-module.exports = { readingTime };
